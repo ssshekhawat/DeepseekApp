63891d12dccd2d862c4a7b06af07bac09222045a
Sandeep Singh Shekhawat <ssshekhawat@users.noreply.github.com>
2025-02-17 16:50:34 +0530

    update

---
+++ deepseek-backend/server.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const DEEPSEEK_API = "http://localhost:11434/";
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/api/deepseek", async(req,res) => {
+    try{
+        const { prompt } = req.body;
+        const response = await axios.post(`${DEEPSEEK_API}/v1/chat/completions`,{
+            model: "deepseek-r1",
+            message: [{role:"user",content: prompt}],
+        });
+        res.json(response.data);
+    }catch(error){
+        console.error("Error:", error.message);
+        res.status(500).json({error:"Failed to communicate with Deepseek-R1"})
+    }
+});
+
+app.listen(PORT, ()=>console.log(`Server running on http://localhost:${PORT}`));
